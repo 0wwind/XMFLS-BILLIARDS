3bb2abf2fe1f1d19ec6fbe182b813adb5182b030
0wwind <0wind@proton.me>
2024-03-03 02:54:46 +0800

    build

---
+++ docs/.vitepress/config.mjs
@@ -0,0 +1,53 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "XMFLS-BILLIARDS",
+  description: "XMFLS-BILLIARDS WEBSITE",
+  head: [
+    ['link', { rel: 'icon', href: '/img/XMFLS.png' }],
+  ],
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    logo: "./img/XMFLS.png", // 页面上显示的logo
+    nav: [
+      { text: '主页', link: '/' },
+      { text: '规则', link: '/rules/厦外台球室管理制度.md' },
+      { text: '段赛', link: '/race/' },
+      { text: '成员', link: '/members/' },
+      { text: '关于我们', link: '/aboutUs/' },
+    ],
+
+    sidebar:
+      {
+        '/rules/': [
+          {
+            text: '规则',
+            items: [
+              { text: '厦外台球室管理制度', link: '/rules/厦外台球室管理制度.md' },
+              { text: '台球桌基本结构示意图', link: '/rules/台球桌基本结构示意图.md' },
+              { text: '八球基本规则及常用细则介绍', link: '/rules/八球基本规则及常用细则介绍.md' },
+              { text: '九球基本规则及常用细则介绍', link: '/rules/九球基本规则及常用细则介绍.md' },
+            ]
+          }
+        ],
+      },
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/0wwind/XMFLS-BILLIARDS' }
+    ]
+  },
+  footer: {
+    message: 'Released under the MIT License.',
+    copyright: 'Copyright © 2024 XMFLS-BILLIARDS'
+  },
+  docFooter: {
+    prev: '上一页',
+    next: '下一页'
+  },
+  darkModeSwitchLabel:'亮/暗模式',
+  lightModeSwitchTitle:'调整为亮色模式',
+  darkModeSwitchTitle:'调整为暗色模式',
+  sidebarMenuLabel:'菜单',
+  returnToTopLabel:'回到顶部',
+})
---
+++ docs/index.md
@@ -0,0 +1,36 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+title: "XMFLS-BILLIARDS"
+titleTemplate: "厦门外国语学校台球社"
+
+hero:
+  name: "XMFLS-BILLIARDS"
+  text: "厦门外国语学校台球社"
+  tagline: "台球段赛报名已开放"
+  image:
+    src: ./img/logo.png
+    alt: logo
+  actions:
+    - theme: brand
+      text: 报名段赛
+      link: https://www.wjx.cn/vm/thxuI88.aspx
+    - theme: alt
+      text: 段赛信息
+      link: ./race/
+      
+features:
+  - icon: 🧑
+    title: 成员
+    details: 你可以在这里了解我们的往届成员
+    link: ./members/
+  - icon: ⚠️
+    title: 规则
+    details: 你可以在这里了解台球规则及台球室管理条例
+    link: ./rules/厦外台球室管理制度.md
+  - icon: 🛠️
+    title: 开发团队
+    details: 这是维护本此网站的团队寄贡献者
+    link: ./aboutUs/
+---
